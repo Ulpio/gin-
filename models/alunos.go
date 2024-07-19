00a6abd1089b2d7fea729e875a52ebc55d07b3c3
Ulpio <ulpionetto0@gmail.com>
2024-07-19 13:38:19 -0300

    Adicionado regex de CPF e RG

--- models/alunos.go
+++ models/alunos.go
@@ -8,9 +8,9 @@ import (
 type Aluno struct {
 	gorm.Model
 	Nome string `json:"nome" validate:"nonzero,min=3,max=100"`
-	CPF  string `json:"cpf"  validate:"len=11"`
+	CPF  string `json:"cpf"  validate:"max=15,min=11, regexp=^[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}\\-?[0-9]{2}$"`
 	RG   string `json:"rg"   validate:"max=9"`
-}
+}	
 
 func Validator(aluno *Aluno) error {
 	if err := validator.Validate(aluno); err != nil {
