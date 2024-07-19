6cc251028f44f7d43848d098a046076b703238f0
Ulpio <ulpionetto0@gmail.com>
2024-07-19 20:55:02 -0300

    Criação do teste de status code

--- main_test.go
+++ main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/Ulpio/gin-api-golang/controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +14,13 @@ func SetupTestRoutes() *gin.Engine {
 	return rotas
 }
 
-func TestFalha(t *testing.T) {
-	t.Fatalf("Teste falhou, pois foi forçado a falhar")
+func TestStatusCode(t *testing.T){
+	r := SetupTestRoutes() // SetupTestRoutes() é uma função que retorna um objeto do tipo *gin.Engine
+	r.GET("/alunos", controllers.ExibeTodosAlunos) // Adiciona a rota /alunos ao objeto r
+	req, _ := http.NewRequest("GET", "/alunos", nil) // Cria uma requisição do tipo GET para a rota /alunos
+	resp := httptest.NewRecorder() // Cria um objeto do tipo *httptest.ResponseRecorder
+	r.ServeHTTP(resp, req) // Executa a requisição
+	if resp.Code != http.StatusOK { // Verifica se o código de status da resposta é 200
+		t.Errorf("Status code should be 200, but got %d", resp.Code) // Se não for, exibe uma mensagem de erro
+		
 }
