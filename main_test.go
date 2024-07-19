559195eb2fe3e19689013d7157c640a069cfaaf3
Ulpio <ulpionetto0@gmail.com>
2024-07-19 20:55:35 -0300

    Criação do teste de status code!

--- main_test.go
+++ main_test.go
@@ -14,13 +14,13 @@ func SetupTestRoutes() *gin.Engine {
 	return rotas
 }
 
-func TestStatusCode(t *testing.T){
-	r := SetupTestRoutes() // SetupTestRoutes() é uma função que retorna um objeto do tipo *gin.Engine
-	r.GET("/alunos", controllers.ExibeTodosAlunos) // Adiciona a rota /alunos ao objeto r
+func TestStatusCode(t *testing.T) {
+	r := SetupTestRoutes()                           // SetupTestRoutes() é uma função que retorna um objeto do tipo *gin.Engine
+	r.GET("/alunos", controllers.ExibeTodosAlunos)   // Adiciona a rota /alunos ao objeto r
 	req, _ := http.NewRequest("GET", "/alunos", nil) // Cria uma requisição do tipo GET para a rota /alunos
-	resp := httptest.NewRecorder() // Cria um objeto do tipo *httptest.ResponseRecorder
-	r.ServeHTTP(resp, req) // Executa a requisição
-	if resp.Code != http.StatusOK { // Verifica se o código de status da resposta é 200
+	resp := httptest.NewRecorder()                   // Cria um objeto do tipo *httptest.ResponseRecorder
+	r.ServeHTTP(resp, req)                           // Executa a requisição
+	if resp.Code != http.StatusOK {                  // Verifica se o código de status da resposta é 200
 		t.Errorf("Status code should be 200, but got %d", resp.Code) // Se não for, exibe uma mensagem de erro
-		
+	}
 }
