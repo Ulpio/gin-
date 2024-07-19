581d56eddbdc3d0776eabe25b47f9812aadb123a
Ulpio <ulpionetto0@gmail.com>
2024-07-19 12:21:08 -0300

    Validando informações

--- controllers/controller.go
+++ controllers/controller.go
@@ -21,6 +21,11 @@ func CriaNovoAluno(c *gin.Context) {
 			"error": err.Error()})
 		return
 	}
+	if err := models.Validator(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
 	database.DB.Create(&aluno)
 	c.JSON(http.StatusOK, aluno)
 }
@@ -34,4 +39,48 @@ func ExibeAlunoPorID(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, aluno)
+}
+
+func DeleteAluno(c *gin.Context) {
+	var id = c.Params.ByName("id")
+	var aluno models.Aluno
+	database.DB.First(&aluno, id)
+	if aluno.Nome == "" {
+		c.JSON(http.StatusNotFound, gin.H{"data": "Aluno não encontrado"})
+		return
+	}
+	database.DB.Delete(&aluno)
+	c.JSON(http.StatusOK, gin.H{"data": "Aluno deletado com sucesso"})
+}
+
+func EditaAluno(c *gin.Context) {
+	var id = c.Params.ByName("id") //Recebe o parametro id da URL
+	var aluno models.Aluno         //Cria uma variável do tipo Aluno
+	database.DB.First(&aluno, id)  //Busca o aluno no banco de dados
+	if aluno.Nome == "" {          //Se o aluno não for encontrado
+		c.JSON(http.StatusNotFound, gin.H{"data": "Aluno não encontrado"}) //Retorna um erro
+		return                                                             //Encerra a função
+	}
+	if err := models.Validator(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	if err := c.ShouldBindJSON(&aluno); err != nil { //Se houver erro ao fazer o bind do JSON
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) //Retorna um erro
+		return                                                     //Encerra a função
+	}
+	database.DB.Save(&aluno)     //Salva as alterações no banco de dados
+	c.JSON(http.StatusOK, aluno) //Retorna o aluno alterado
+}
+
+func GetAlunoPeloCPF(c *gin.Context) {
+	var cpf = c.Params.ByName("cpf")                //Recebe o parametro cpf da URL
+	var aluno models.Aluno                          //Cria uma variável do tipo Aluno
+	database.DB.Where("cpf = ?", cpf).First(&aluno) //Busca o aluno no banco de dados
+	if aluno.Nome == "" {                           //Se o aluno não for encontrado
+		c.JSON(http.StatusNotFound, gin.H{"data": "Aluno não encontrado"}) //Retorna um erro
+		return                                                             //Encerra a função
+	}
+	c.JSON(http.StatusOK, aluno) //Retorna o aluno encontrado
 }
--- routes/router.go
+++ routes/router.go
@@ -10,5 +10,8 @@ func HandleRequests() {
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.GET("/alunos/:id", controllers.ExibeAlunoPorID)
+	r.DELETE("/alunos/:id", controllers.DeleteAluno)
+	r.PUT("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.GetAlunoPeloCPF)
 	r.Run()
 }
