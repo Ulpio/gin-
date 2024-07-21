a68b306713cd394aad44a4343092303594ca69b6
ulpio <ulpionetto0@gmail.com>
2024-07-21 01:33:25 +0000

    Criado rotas para html

--- routes/router.go
+++ routes/router.go
@@ -7,6 +7,8 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
 	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.GET("/saudacao/:nome", controllers.Saudacao)
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
@@ -14,6 +16,7 @@ func HandleRequests() {
 	r.GET("/alunos/:id", controllers.ExibeAlunoPorID)
 	r.PUT("/alunos/:id", controllers.EditaAluno)
 	r.DELETE("/alunos/:id", controllers.DeleteAluno)
-
+	r.GET("/", controllers.ShowIndex)
+	r.NoRoute(controllers.Rota404)
 	r.Run()
 }
